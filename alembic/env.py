6c883ed8eb3a7c53612bdd9911456f12fbd8e301
Bagher Salahshoor <baghersalahshoor@gmail.com>
2025-12-26 19:37:51 +0330

    chore: configure alembic to use app settings and models

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,6 @@
 import asyncio
+import sys
+from os.path import abspath, dirname
 from logging.config import fileConfig
 
 from sqlalchemy import pool
@@ -7,20 +9,31 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+from app.core.config import settings
+from app.core.database import Base
+from app.models.coin import Coin
+from app.models.coin_price import CoinPrice
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URL))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+    
+
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
